fc8c40dfc65cf16d2f7bfaa884e4d7ea5a632e97
Ben Merideth <benmerideth@Mac.lan>
2025-11-01 18:16:24 -0700

    added a repl function

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,35 @@ mod compiler;
 mod error;
 
 use vm::{VM};
+use std::io::{stdin, stdout, Write};
+
+fn repl() {
+    let mut line = String::new();
+
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+
+        line.clear();
+        let bytes = stdin().read_line(&mut line).unwrap();
+        if bytes == 0 {
+            break;
+        }
+
+        let input = line.trim();
+        if input.eq_ignore_ascii_case("quit") || input.eq_ignore_ascii_case("exit") {
+            break;
+        }
+
+        match VM::interpret(input) {
+            Err(e) => println!("{:?}", e),
+            Ok(value) => println!("{:?}", value),
+        }
+    }
+
+    println!("Exiting.");
+}
 
 fn main() {
-    let src = "1 + 2";
-
-    match VM::interpret(src) {
-        Err(e) => println!("{:?}", e),
-        Ok(value) => println!("{:?}", value),
-    }
+    repl();
 }
